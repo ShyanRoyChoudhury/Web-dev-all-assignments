2cac46c388ae68ae7ac2d5e57ff087fa7b639272
ShyanRoyChoudhury <Shayan.roy31@gmail.com>
2023-08-22 02:00:56 +0530

    created initial boilerplate on hard assignment.

---
+++ week-4/03-hard-course-selling-app-user-side/course-selling-app-user-side/src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-outer-dom';
+import Landing from './components/Landing';
+function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element = ''/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App;
+
