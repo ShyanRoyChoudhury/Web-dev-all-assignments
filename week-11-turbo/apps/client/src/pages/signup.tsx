024664e85e64531b851424fe961154e565eef799
ShyanRoyChoudhury <Shayan.roy31@gmail.com>
2023-10-05 17:42:48 +0530

    turborepo first attempt

---
+++ week-11-turbo/apps/client/src/pages/signup.tsx
@@ -0,0 +1,8 @@
+import { Signup } from "ui";
+
+export default function SignupPage(){
+    return (<>
+        hi from signup
+        <Signup/>
+    </>)
+}
---
+++ week-11-turbo/packages/ui/components/Signup.tsx
@@ -0,0 +1,7 @@
+export function Signup(){
+    return<>
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button>Submit</button>
+    </>
+}
