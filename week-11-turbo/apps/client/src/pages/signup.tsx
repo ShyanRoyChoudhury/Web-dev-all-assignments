33556073aca655ae08016429aab96ea378e983f3
ShyanRoyChoudhury <Shayan.roy31@gmail.com>
2023-10-06 02:13:32 +0530

    adding backend packages

--- week-11-turbo/apps/client/src/pages/signup.tsx
+++ week-11-turbo/apps/client/src/pages/signup.tsx
@@ -3,6 +3,11 @@ import { Signup } from "ui";
 export default function SignupPage(){
     return (<>
         hi from signup
-        <Signup/>
+        <Signup onClick = {async (username, password)=>{
+            const response = await axios.post('/admin/signup', {
+                username,
+                password
+            }) 
+        }}/>   
     </>)
 }
